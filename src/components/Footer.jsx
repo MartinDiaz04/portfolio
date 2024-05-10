269a1f978609fc6d67b481d1ff247c79140d87cc
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-05-10 20:55:57 -0300

    create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { data } from "../data/data";
+import { CiMail } from "react-icons/ci";
+import { CiPhone } from "react-icons/ci";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaRegFilePdf } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="text-center bg-black">
+      <div className="flex items-center justify-center">
+        <a href={data.urlLinkedIn} className="text-white text-5xl mx-4">
+          <FaLinkedin />
+        </a>
+        <a href={data.urlGitHub} className="text-white text-5xl mx-4">
+          <FaGithub />
+        </a>
+        <a href={`mailto:${data.email}`} className="text-white text-5xl mx-4">
+          <CiMail />
+        </a>
+        <a href={`tel:${data.phone}`} className="text-white text-5xl mx-4">
+          <CiPhone />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
