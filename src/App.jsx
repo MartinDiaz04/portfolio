239d386fc59b3e96d81fcfd7e5ddb44f45f10cbc
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-05-10 20:55:40 -0300

    appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import NavBar from "./components/NavBar";
 import Home from "./screens/Home";
 import Projects from "./screens/Projects";
 import Studies from "./screens/Studies";
-import Contact from "./screens/Contact";
 
 
 function App() {
   return (
     <div>
       <Router>
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/studies" element={<Studies/>} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
