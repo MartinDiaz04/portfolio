76ccea4bb2899198fe086734524a5789c1928c9e
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-05-10 20:56:53 -0300

    change in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,65 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { TbWorld } from "react-icons/tb";
-import { CiMail } from "react-icons/ci";
-import { CiPhone } from "react-icons/ci";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import {data} from '../data/data'
-const links = [
-  {
-    name: "PROYECTOS",
-    href: "/projects",
-  },
-  {
-    name: "EDUCACION",
-    href: "/studies",
-  },
-  {
-    name: "CONTACTO",
-    href: "/contact",
-  },
-];
+import { FaRegFilePdf } from "react-icons/fa";
+import { data } from "../data/data";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const NavBar = () => {
   return (
-    <header className="grid grid-cols-1 items-center justify-center">
-      <figure className="h-64 w-auto mx-5 my-5 flex justify-center items-center">
-        <img src="/img/foto.png" className="rounded-full h-auto w-64"></img>
-      </figure>
-      <h1>hola</h1>
-      <div className="w-auto mx-5 my-5 grid grid-cols-1 justify-center items-center">
-        <h1 className="text-white m-auto font-bold text-5xl mb-2">
-          {data.name}
-        </h1>
-        <h2 className="text-white m-auto text-3xl">
-          {data.label}
-        </h2>
-        <h4 className="text-white m-auto flex items-center text-xl">
-          <TbWorld className="mr-1" /> {data.location}
-        </h4>
-        <div className="flex items-center justify-center mt-2">
-          <a href={data.urlLinkedIn} className="text-white text-2xl mx-1"><FaLinkedin/></a>
-          <a href={data.urlGitHub} className="text-white text-2xl mx-1"><FaGithub/></a>
-          <a href={`mailto:${data.email}`} className="text-white text-2xl mx-1"><CiMail/></a>
-          <a href={`tel:${data.phone}`} className="text-white text-2xl mx-1"><CiPhone/></a>
-        </div>
-      </div>
-      <nav className="w-auto mx-5 ">
-        <ul className="list-none sm:flex sm:items-center sm:justify-center h-auto max-w-xl m-auto">
-          {links.map((x, index) => (
-            <li key={index} className="list-none text-center h-max p-3 mx-3">
+    <div className="flex-wrap gap-5 h-screen">
+      <Header />
+      <main className="bg-black h-auto">
+        <figure className=" w-auto flex justify-center my-10">
+          <img
+            src="/img/foto.png"
+            className="rounded-full h-auto w-44 md:w-64"
+          ></img>
+        </figure>
+        <div className="w-auto mx-5 my-16 grid grid-cols-1">
+          <h1 className="text-white m-auto font-bold text-3xl md:text-5xl mb-2 ">
+            {data.name}
+          </h1>
+          <h2 className="text-white m-auto text-2xl  md:text-3xl">
+            {data.label}
+          </h2>
+          <h4 className="text-white m-auto flex items-center text-lg md:text-xl">
+            <TbWorld className="mr-1" /> {data.location}
+          </h4>
+          <ul className="flex justify-center mt-3">
+            <li>
               <a
-                href={x.href}
-                className="text-white bg-black p-3 hover:bg-gray-900 rounded-xl"
+                href="/CV/CV Martín Díaz Preiero SPANISH.pdf"
+                download="CV Martín Díaz Preiero SPANISH.pdf"
+                className="text-white text-center mt-2 flex items-center"
               >
-                {x.name}
+                <FaRegFilePdf /> Descargar CV
               </a>
             </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+          </ul>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
   );
 };
 
